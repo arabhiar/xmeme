d54dc04a06e30960738aa95dcba7d72164d32291
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-02-21 21:35:08 +0530

    cahnge in dbLocal.js

--- backend/config/dbLocal.js
+++ backend/config/dbLocal.js
@@ -1,31 +1,47 @@
-import low from "lowdb";
-import FileSync from "lowdb/adapters/FileSync.js";
-import shortid from "shortid";
+// this file is used to connect to local database(only if the website is running in development mode)
+// that is stored in json file using lowdb module
 
-const adapter = new FileSync("meme-database.json");
+import low from "lowdb"; // importing lowdb module
+import FileSync from "lowdb/adapters/FileSync.js";
+import shortid from "shortid"; // a utility module used to assign an unique short ID to an object
+
+const adapter = new FileSync("meme-database.json"); // json file where data is stored
 const db = low(adapter);
 
-db.defaults({ memes: [] }).write();
+db.defaults({ memes: [] }).write(); // we are storing all the memes in a array called memes which is stored in json file
+
+// @desc: function used to upload a new meme(pushing a new meme to memes array)
 
 function uploadMeme(meme) {
-  meme._id = shortid.generate();
-  db.get("memes").push(meme).write();
-  return { _id: meme.id };
+  const id = shortid.generate();
+  let { name, url, caption } = meme;
+  const newMeme = { id, name, url, caption };
+  db.get("memes").push(newMeme).write();
+  return { id: newMeme.id };
 }
+
+// @desc: func used to get all the memes stored in memes array.
 
 function getMemes() {
   let data = db.get("memes").value();
   let cloneData = [...data].reverse().slice(0, 100);
+  // console.log(cloneData);
   return cloneData;
 }
 
-function getMemeById(_id) {
-  return db.get("memes").find({ _id }).value();
+// @desc: func used to get a meme by it's ID
+
+function getMemeById(id) {
+  return db.get("memes").find({ id }).value();
 }
 
-function updateMeme(_id, data) {
-  db.get("memes").find({ _id }).assign(data).write();
+// @desc: func used to update a meme
+
+function updateMeme(id, data) {
+  db.get("memes").find({ id }).assign(data).write();
 }
+
+// @desc: func used to delete all meme. It is only used to seed some sample data in json file
 
 function deleteAllMeme() {
   db.get("memes").remove().write();
--- backend/controllers/memeController.js
+++ backend/controllers/memeController.js
@@ -33,19 +33,22 @@ const getMemeById = asyncHandler(async (req, res) => {
 
 const uploadMeme = asyncHandler(async (req, res) => {
   let { name, url, caption } = req.body;
-  name = name.trim();
-  url = url.trim();
-  caption = caption.trim();
+  name = name.trim(); // trimming to remove extra space
+  url = url.trim(); // trimming to remove extra space
+  caption = caption.trim(); // trimming to remove extra space
   if (name && url && caption) {
+    // if meme, url & caption exists after trimming we will check if another meme with same url exists or not
     const memeExists = await Meme.findOne({ url });
     if (memeExists) {
+      // if a meme exists with same error we will throw an error
       res.status(406);
       throw new Error("Meme already exists");
     }
+    // in try catch block we are validating that if the url provided by user is valid or not
+    // if it is valid, it points to an jpeg, png, svg or gif
     try {
-      const { headers, status } = await axios.get(url);
-      const contentType = headers["content-type"];
-      console.log(contentType);
+      const { headers, status } = await axios.get(url); // we send a get request to provided url
+      const contentType = headers["content-type"]; // content-type refers to the type of content send by the sever
       if (
         status === 200 &&
         (contentType === "image/png" ||
@@ -53,30 +56,34 @@ const uploadMeme = asyncHandler(async (req, res) => {
           contentType === "image/gif" ||
           contentType === "image/svg+xml")
       ) {
+        // if content is png, jpeg, gif or svg+xml means we will post that meme to our database
         const meme = await Meme.create({
           name,
           url,
           caption,
         });
         if (meme) {
-          res.status(201).json({ _id: meme._id });
+          res.status(201).json({ id: meme._id });
         } else {
           res.status(500);
           throw new Error("Something went wrong");
         }
       } else {
+        // if content type is not an image or gif we will throw an error
         throw { customError: "Url doesn't point to an image." };
       }
     } catch (err) {
+      // if there is some error in fetching the url means url doesn't exists hence we will throw an error
       if (err.hasOwnProperty("customError")) {
-        res.status(415);
+        res.status(415)
         throw new Error(err.customError);
       } else {
-        res.status(400);
+        res.status(400)
         throw new Error("Url field should contain valid url.");
       }
     }
   } else {
+    // if after trimming, either of values become empty we will throw an error
     res.status(400);
     throw new Error(
       "Please fill all enteries. Ensure that each entry is filled."
@@ -89,29 +96,26 @@ const uploadMeme = asyncHandler(async (req, res) => {
 // @access: Public
 
 const updateMeme = asyncHandler(async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  const id = req.params.id;
+  const id = req.params.id; // it is the parameter corresponding to requested meme for updation
   let { url, caption } = req.body;
 
-  let urlOK = false;
-  let captionOK = false;
+  let urlOK = false; // it indicates if url paramater is valid for updation or not
+  let captionOK = false; // it indicates if caption parameter is valid for updation or not
   if (url) {
-    url = url.trim();
+    // if url parameter exists in the body then we must validate it
+    url = url.trim(); // trimming the url
     if (url) {
-      // console.log("URL entry exists");
+      // if url exists after trimming we will check for if another meme with same url exists or not
       let memeExists = await Meme.findOne({ url });
       if (memeExists && memeExists._id.toString() !== id) {
+        // if meme exists and the id of found meme is not equal to current id, we will throw an error
         res.status(406);
         throw new Error("Meme already exists");
       }
+      // in try catch block we are validating that if the url provided by user is valid or not
+      // if it is valid, it points to an jpeg, png, svg or gif
       try {
-        // console.log("url: ", url);
-        // let data = await axios.get(url);
-        // console.log("Data: ", data);
         const { headers, status } = await axios.get(url);
-        // console.log("headers: ", headers);
-        // console.log("status: ", status);
         const contentType = headers["content-type"];
         console.log(contentType);
         if (
@@ -121,11 +125,14 @@ const updateMeme = asyncHandler(async (req, res) => {
             contentType === "image/gif" ||
             contentType === "image/svg+xml")
         ) {
+          // if content-type is image or png we will flag urlOK as true
           urlOK = true;
         } else {
+          // else the url doesn't point to an image, hence we will throw an error
           throw { customError: "Url doesn't point to an image." };
         }
       } catch (err) {
+        // if there is some error while sending request to uel, it means url doesn't exist
         if (err.hasOwnProperty("customError")) {
           res.status(415)
           throw new Error(err.customError);
@@ -135,32 +142,37 @@ const updateMeme = asyncHandler(async (req, res) => {
         }
       }
     } else {
+      // if after trimming url is empty, we will throw an error
       res.status(400)
       throw new Error("Url field can't be an empty string");
     }
   } else {
+    // since url parameter doesn't exist in the body, we are good to go for updation
     urlOK = true;
   }
   if (caption) {
-    caption = caption.trim();
+    caption = caption.trim(); // trimming the caption
     if (caption) {
+      // if after trimming caption exists it means we are good to go for updation and hence we will make captionOK as true
       captionOK = true;
     } else {
+      // else we will throw an error since only spaces were provided as caption
       res.status(400)
       throw new Error("Caption field can't be an empty string");
     }
   } else {
+    // since caption parameter doesn't exist in the body, we are good to go for updation
     captionOK = true;
   }
-  const meme = await Meme.findById(req.params.id);
+  const meme = await Meme.findById(req.params.id); // checking if the meme with given ID exists or not
   if (meme && captionOK && urlOK) {
-    // console.log(meme);
+    // if meme exists, captionOK is true and urlOK is true, it means everything is right and we will update the data
     meme.url = url;
     meme.caption = caption;
-    // console.log(meme);
     await meme.save();
     res.sendStatus(201);
   } else {
+    // if meme doesn't exist we will throw an error
     res.status(404);
     throw new Error("Meme not found!");
   }
